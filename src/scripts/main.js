8b2abf27592c5cdf8b27c71cf3eeb94213ea1f13
Rose Wisotzky <rose@rosewisotzky.com>
2019-04-01 17:19:13 -0500

    First commit

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+import sayHello from "./contact"
+import sayGoodbye from "./goodbye"
+import SandwichMaker from "./contactList"
+
+sayHello()
+sayGoodbye()
+
+SandwichMaker.placeOrder("rye", "capicola", "provolone")
